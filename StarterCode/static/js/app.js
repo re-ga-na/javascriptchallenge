9d8ec129fe83388dc978fadbb0c643c7678ccbb4
R Alicka <ralicka@rs-macbook-air.home>
2020-04-04 22:42:18 -0400

    complete

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,38 @@
+  
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// selecting table body w d3
+var tbody = d3.select("tbody");
+
+// table setup
+tableData.forEach(function(x) {
+    var row = tbody.append("tr");
+    Object.entries(x).forEach(function([key, value]) {
+    var cell = row.append("td");
+    cell.text(value);
+    });
+});
+
+// creating filter button and function
+var button = d3.select("#filter-btn");
+  
+button.on("click", function()
+{
+  d3.event.preventDefault();
+    //clear table
+    tbody.text("");
+    //filter using input date
+    var dateInput = d3.select("#datetime");
+    var dateValue = dateInput.property("value");
+    console.log(dateValue);
+    var filteredData = tableData.filter(x => x.datetime === dateValue);
+    console.log(filteredData);
+    filteredData.forEach(function(x) {
+        var row = tbody.append("tr");
+        Object.entries(x).forEach(function([key, value]) {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });
+});   
